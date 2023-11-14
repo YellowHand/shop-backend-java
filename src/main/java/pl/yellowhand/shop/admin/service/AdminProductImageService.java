7b745b5753493722cf2d892f9ea316b70aba8542
pawel <ypawelborkowski@gmail.com>
2023-11-14 18:10:11 +0100

    upload image

--- src/main/java/pl/yellowhand/shop/admin/service/AdminProductImageService.java
+++ src/main/java/pl/yellowhand/shop/admin/service/AdminProductImageService.java
@@ -20,7 +20,7 @@ public class AdminProductImageService {
 
     public String uploadImage(String filename, InputStream inputStream) {
         String newFileName = UploadedFilesNameUtils.slugifyFileName(filename);
-
+        newFileName = ExistingFileRenameUtils.renameIfExists(Path.of(uploadDir), newFileName);
 
         Path filePath = Paths.get(uploadDir).resolve(newFileName);
         try (OutputStream outputStream = Files.newOutputStream(filePath);){
--- src/main/java/pl/yellowhand/shop/admin/service/UploadedFilesNameUtils.java
+++ src/main/java/pl/yellowhand/shop/admin/service/UploadedFilesNameUtils.java
@@ -3,11 +3,11 @@ package pl.yellowhand.shop.admin.service;
 import com.github.slugify.Slugify;
 import org.apache.commons.io.FilenameUtils;
 
-public class UploadedFilesNameUtils {
+class UploadedFilesNameUtils {
 
     public static String slugifyFileName(String filename) {
         String name = FilenameUtils.getBaseName(filename);
-        Slugify slg = Slugify.builder().build();
+        Slugify slg = Slugify.builder().customReplacement("_", "-").build();
         String changedName = slg.slugify(name);
         return changedName + "." + FilenameUtils.getExtension(filename);
     }
